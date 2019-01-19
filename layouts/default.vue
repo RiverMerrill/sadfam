dfe2e9ed8ef1b74a70ec86b593be3413769d952e
River Merrill <rivermerrill.me@gmail.com>
2019-01-19 00:23:20 -0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation">
+      <div class="navbar-brand">
+        <nuxt-link
+          to="songs"
+          class="navbar-item">
+          <img
+            src="~assets/buefy.png"
+            alt="Buefy"
+            height="28">
+        </nuxt-link>
+        <nuxt-link 
+          v-for="(item, key) of items"
+          :key="key" 
+          :to="item.to"
+          class="navbar-item">
+          {{ item.title }}
+        </nuxt-link>
+        <div class="navbar-burger">
+          <span/>
+          <span/>
+          <span/>
+        </div>
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: 'Songs', to: { name: 'songs' } },
+        { title: 'songwriters', to: { name: 'songwriters' } },
+        { title: 'mission', to: { name: 'mission' } },
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/songs.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="section">
+    <div class="container columns is-mobile">
+      <div>
+        <div class="card">
+          <div class="card-header">
+            <p class="cared-header-title has-text-grey">
+              Song Title
+            </p>
+            <br>
+            <p class="card-header-title username">UserName</p>
+          </div>
+          <div class="card-content">
+            <div class="content has-text-centered">
+              Song description Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure quo consectetur debitis adipisci soluta vel ipsum hic tenetur. Veritatis, unde!
+            </div>
+          </div>
+          <footer class="card-footer">
+            <p>Comment section (input goes here)</p>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import BLogo from '@/components/Logo'
+
+export default {
+  name: 'Songs',
+  components: { BLogo },
+  data() {
+    return {
+      features: [
+        { icon: 'github-circle', title: 'Free', content: `<span>Open source on <a href="https://github.com/buefy/buefy"> GitHub</a></span>` },
+        { icon: 'cellphone-link', title: 'Responsive', content: `<span><b class="has-text-grey">Every</b> component is responsive</span>` },
+        { icon: 'alert-decagram', title: 'Modern', content: `<span>Built with <a href="https://vuejs.org/">Vue.js</a> and <a href="http://bulma.io/">Bulma</a></span>` },
+        { icon: 'arrange-bring-to-front', title: 'Lightweight', content: `<span>No other internal dependency</span>` }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.username {
+  color: green;
+  margin-left: 20px;
+}
+</style>
+
